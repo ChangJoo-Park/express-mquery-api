5174d17c770b659b52d99e833378cd29dbabc587
ChangJoo Park <pcjpcj2@gmail.com>
2018-06-13 12:40:37 +0900

    Add first route /api/users

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const bodyParser = require('body-parser')
-const mongoose = require('./db')
 const mquery = require('express-mquery')
 const morgan = require('morgan')
-
+const mongoose = require('./db')
+const models = require('./models')
 const app = express()
 
 app.use(morgan('combined'))
@@ -12,6 +12,19 @@ app.use(mquery({
   limit: 10,
   maxLimit: 50
 }))
+
+// Set Routes
+app.get('/api/users', (request, response, next) => {
+  const {
+    User
+  } = models
+  const options = request.mquery
+  User
+    .get(options, (error, results) => {
+      if (error) response.json(error)
+      response.json(results)
+    })
+})
 
 mongoose.connect('mongodb://localhost/myapp').then(_ => {
   console.log('connect to mongoDB Successfully')
