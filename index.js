31c3e0c00a3245caaf38283ebb6c1dbdcd77882e
ChangJoo Park <pcjpcj2@gmail.com>
2018-06-14 23:01:09 +0900

    Add post show / post,comment create route

--- index.js
+++ index.js
@@ -53,9 +53,46 @@ router.post('/signin', async (request, response, next) => {
     email,
     password
   } = request.body
-  User.signIn(email, password).then((user) => {
-    response.json(user)
-  })
+
+  User
+    .signIn(email, password)
+    .then((user) => response.json(user))
+    .catch(e => response.status(500).json(e.message))
+})
+
+router.get('/posts/:post_id', (request, response, next) => {
+  const {
+    Post
+  } = models
+  Post.findById(request.params['post_id'])
+    .populate('author')
+    .populate('comments')
+    .populate({
+      path: 'comments.author',
+      select: 'username email isAdmin'
+    })
+    .then(result => response.json(result))
+    .catch(err => response.status(500).json(err))
+})
+
+router.post('/posts/:post_id/comments', async (request, response, next) => {
+  const {
+    Post
+  } = models
+  const {
+    authorId: author,
+    body
+  } = request.body
+  const p = await Post.findById(request.params['post_id'])
+
+  p.addComment({
+      author: {
+        _id: author
+      },
+      body
+    })
+    .then(result => response.json(result))
+    .catch(err => response.status(500).json(err))
 })
 
 router.get('/posts', (request, response, next) => {
@@ -82,11 +119,24 @@ router.post('/posts', (request, response, next) => {
   } = request.body
 
   Post.create(authorId, title, body)
-    .then((result) => {
-      response.json(result)
-    }).catch((err) => {
-      response.status(500).json(err)
-    })
+    .then(result => response.json(result))
+    .catch(err => response.status(500).json(err))
+})
+
+router.post('/comments', (request, response, next) => {
+  const {
+    Comment
+  } = models
+
+  const {
+    authorId,
+    postId,
+    body
+  } = request.body
+
+  Comment.create(authorId, postId, body)
+    .then(result => response.json(result))
+    .catch(err => response.status(500).json(err))
 })
 
 app.use('/api/v1', router)
